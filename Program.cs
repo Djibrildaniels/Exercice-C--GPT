085e285b241444b6b252d8be2b5c5ade4897b9a0
Djibrildaniels <ed.mensah89@gmail.com>
2024-06-13 17:20:41 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Exercice Facile :
+            // Écris un programme qui affiche la table de multiplication de 1 à 10 pour un nombre saisi par l'utilisateur.
+
+
+            // Console.WriteLine("Veuillez entrer un chiffre:");
+
+            // int multiplication = Convert.ToInt32(Console.ReadLine());
+
+            // int i = 1;
+
+            // while (i <= 10)
+            // {
+            // Console.WriteLine(multiplication + " * " + i + " = " + (multiplication *     i));
+            //      i++;
+            // }
+
+
+
+// Écris un programme qui demande à l'utilisateur de saisir un nombre entier positif et qui affiche tous les nombres de Fibonacci jusqu'à ce nombre en utilisant une boucle for.
+
+
+
+            // Console.WriteLine("Veuillez saisir un chiffre:");
+
+            // int nombre = Convert.ToInt32(Console.ReadLine());
+
+            // int a = 0;
+            // int b = 1;
+
+            // Console.Write("Les nombres de Fibonacci jusqu'à " + nombre + " sont: " + a + " " + b + " ");
+
+            // for (int i = 2; i < nombre; i++)
+            // {
+            //     int c = a + b;
+            //     Console.Write(c + " ");
+            //     a = b;
+            //     b = c;
+            // }
+
+            // Console.WriteLine();
+
+    
+
+ for (int i = 1; i <= 10; i++)
+ {
+    Console.WriteLine("Voici les chiffres : " + i );
+  
+    
+
+ }
+
+            
+            
+
+
+        
+
+
+
+      
